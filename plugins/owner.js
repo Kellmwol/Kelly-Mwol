55ad74fcc833f208e87a89d45ca5e46914cb3629
Shefin-Sir <95514870+Shefin-Sir@users.noreply.github.com>
2021-12-04 10:28:56 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -30,7 +30,7 @@ else if (Config.WORKTYPE == 'public') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*Amalser by Farhan_Dqz 💌 Amal*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*Kelly Mwol by Shefin Sir*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.OWNER + '\n\n--------------------', MessageType.text);
